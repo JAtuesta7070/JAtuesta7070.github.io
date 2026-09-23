c240f540e2d476093d5612d10057907a36f5bf0b
JAtuesta7070 <jatuest7070@student.gwinnetttech.edu>
2026-09-22 22:17:20 -0400

    Lesson 5 Assignment
    I am struggling with the problem for my NPM and try to follow everything by the books.

---
+++ Lesson 5/src/App.jsx
@@ -0,0 +1,24 @@
+function App() { 
+
+ return ( 
+
+   <div> 
+
+     <h1>Hello React!</h1> 
+
+     <p>This is your first React component.</p> 
+
+   </div> 
+
+ ); 
+
+} 
+
+//What does npm run dev do?
+// It help give the code to allow us to become a developer for our site.//
+
+//What is the purpose of vite.config.js?//
+// It configures the Vite build tool for the project.//
+
+//What happens when you run npm run build?//
+// It creates a production-ready version of the application in the dist folder.//
